e7d8f45f4eab078e17cff45e07d38717c6799b66
digweed67 <124364643+digweed67@users.noreply.github.com>
2024-12-11 13:11:46 +0100

    basic skeleton for SearchBar

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,40 @@
+import React, { useState }  from "react"; 
+
+
+function SearchBar() {
+    const [userInput, setUserInput] = useState("");
+
+    const handleChange = (e) => {
+
+        setUserInput(e.target.value);
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        // fetch data from API
+
+    }
+
+
+    return (
+        <div className="search-bar-container">
+            <input 
+                type="text" 
+                id="userInput" 
+                value={userInput} 
+                onChange={handleChange} 
+                placeholder="Search for songs"
+            />
+
+            <button 
+                type="submit" 
+                id="button" 
+                onClick={handleClick}
+            >
+                Search
+            </button>
+        </div>
+    );
+};
+
+export default SearchBar; 
