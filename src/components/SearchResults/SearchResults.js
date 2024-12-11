418d72c0ffedfab022657251f6fb5c3242c4542d
digweed67 <124364643+digweed67@users.noreply.github.com>
2024-12-11 14:01:19 +0100

    create skeleton for SearchResults with mock array data

---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Tracklist from "./Tracklist";
+import Track from "./Track";
+
+
+
+function SearchResults() {
+    const tracks = [
+        {
+            id: "1",
+            name: "Last Christmas",
+            artist: "Wham!",
+            album: "Last Christmas - The Original"
+        },
+        {
+            id: "2",
+            name: "All I Want for Christmas Is You",
+            artist: "Mariah Carey",
+            album: "Merry Christmas"
+        },
+        {
+            id: "3",
+            name: "Do They Know It's Christmas?",
+            artist: "Band Aid",
+            album: "Do They Know It's Christmas?(Single)"
+        },
+        {
+        id: "4",
+        name: "Happy Xmas (War Is Over)",
+        artist: "John Lennon & Yoko Ono",
+        album: "Happy Xmas"
+        }
+        
+    
+    ];
+
+
+    return (
+        <div className="result-list">
+            <Tracklist song={tracks}/>
+        </div>
+    );
+}
+
+export default SearchResults; 
